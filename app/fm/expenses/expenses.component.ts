e650f50edf3a821891393d127670d9be45cc0a56
HaoHo <haho@codix.eu>
2017-08-04 16:39:56 +0700

    Add scripts of lc-datepicker.

--- app/fm/expenses/expenses.component.ts
+++ app/fm/expenses/expenses.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {Router} from "@angular/router";
 import {ModalComponent} from "../../common/modal/modal.component";
 import {Expense} from "./expense";
@@ -8,6 +8,8 @@ import {PaymentMethod} from "./paymentMethod";
 import {Observable} from "rxjs/Rx";
 import {ExpensesDetailsPresenter} from "./expensesDetailsPresenter";
 import {EventExpenseService} from "./eventExpenses/eventExpense.service";
+import {LCDatePickerComponent} from "../../lib/lc-date-picker/lc-date-picker.component";
+import {DatePickerConfig, ECalendarType} from "../../lib/lc-date-picker/lc-date-picker-config-helper";
 
 @Component({
   moduleId: module.id,
@@ -23,6 +25,13 @@ export class ExpensesComponent implements OnInit {
   expenseEdit: Expense = new Expense();
   @ViewChild(ModalComponent) modal: ModalComponent;
   idUpdate: number;
+  @ViewChild('calendar')
+  calendar: LCDatePickerComponent;
+
+  @ViewChild('dateInput')
+  dateInput: ElementRef;
+  public config = new DatePickerConfig();
+  public CalendarOpened = false;
 
   constructor(
     private _expensesService: ExpensesService,
@@ -31,6 +40,33 @@ export class ExpensesComponent implements OnInit {
     private fb: FormBuilder,
   ) {
     this.pageTitle = 'Expenses';
+
+    this.config.CalendarType = ECalendarType.Date;
+    this.config.Localization = 'hr';
+    this.config.MinDate = { years: 1900 };
+    this.config.MaxDate = { years: 2100 };
+    this.config.Labels = {
+      confirmLabel: 'Ok',
+    };
+
+    this.config.PrimaryColor = '#5e666f';
+    this.config.FontColor = '#5e666f';
+  }
+
+  public openCalendar() {
+    this.CalendarOpened = !this.CalendarOpened;
+  }
+
+  public clearCalendar() {
+    this.dateInput.nativeElement.value = '';
+  }
+
+  public get setDate() {
+    return this.dateInput.nativeElement.value;
+  }
+
+  public set setDate(value) {
+    this.dateInput.nativeElement.value = value;
   }
 
   ngOnInit(): void {
